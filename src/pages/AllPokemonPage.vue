202102723859ca7ecf8f012d34c49307b8bcb2e3
Christian Jiménez Roche <christianjimenezroche@gmail.com>
2024-03-18 14:11:06 +0100

    All pokemon page
    Fetching the pokemons, and then we're showing them through the pokemon list component

--- src/pages/AllPokemonPage.vue
+++ src/pages/AllPokemonPage.vue
@@ -1,5 +1,25 @@
 <template>
-  <q-page padding> </q-page>
+  <q-page padding>
+    <q-infinite-scroll @load="loadPokemons">
+      <list-pokemon :pokemons="store.pokemonData" />
+      <template #loading>
+        <div class="row justify-center q-my-md">
+          <q-spinner-dots color="primary" size="40px" />
+        </div>
+      </template>
+    </q-infinite-scroll>
+  </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ListPokemon from 'src/components/ListPokemon.vue';
+import { usePokemonStore } from 'src/stores/pokemon-store';
+import { onMounted } from 'vue';
+
+const store = usePokemonStore();
+
+const loadPokemons = async (index: number, done: CallableFunction) => {
+  await store.loadPokemonDataGrouped(10, index);
+  done();
+};
+</script>
