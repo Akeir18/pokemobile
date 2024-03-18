ed9034c08cf40fb8c8030bb0a93671a7345ab155
Christian Jiménez Roche <christianjimenezroche@gmail.com>
2024-03-18 14:09:58 +0100

    Creating Pokemon list component
    The component that given the array of pokemon objects, shows the list

---
+++ src/components/ListPokemon.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-list bordered separator>
+    <q-item v-for="pokemon in pokemons" :key="pokemon.id">
+      <q-item-section avatar>
+        <q-avatar>
+          <img :src="pokemon.sprites.front_default" />
+        </q-avatar>
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>
+          {{ pokemon.id }}
+          {{ pokemon.name }}
+          <q-chip v-for="typeInfo in pokemon.types" :key="typeInfo.slot">
+            {{ typeInfo.type.name }}
+          </q-chip>
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script setup>
+import { defineProps, toRefs } from 'vue';
+const props = defineProps({
+  pokemons: {
+    type: Object,
+    required: true,
+  },
+});
+const { pokemons } = toRefs(props);
+</script>
