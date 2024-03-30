d8a965a6dd0e3b7254c2f656cb4775b5d8e16845
Christian Jiménez Roche <christianjimenezroche@gmail.com>
2024-03-30 07:29:20 +0100

    Creating the theme selector component

---
+++ src/components/ThemeSelector.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-btn icon="contrast" flat @click="$q.dark.toggle()" round />
+</template>
+
+<script setup lang="ts">
+import { useQuasar } from 'quasar';
+
+const $q = useQuasar();
+</script>
