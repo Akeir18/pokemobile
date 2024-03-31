51eca61b08a375bc5fa7f4e330d3d4fa6538c1f1
Christian Jiménez Roche <christianjimenezroche@gmail.com>
2024-03-31 07:19:33 +0200

    Making the pokemon page, at least, show the pokemon picture

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,5 +1,26 @@
 <template>
-  <h1>New component</h1>
+  <q-page padding>
+    <div v-if="pokemon !== undefined">
+      <q-img :src="pokemon.sprites.front_default" />
+    </div>
+  </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import usePokemonStore from 'src/stores/pokemon-store';
+import { ref } from 'vue';
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const store = usePokemonStore();
+
+const pokemon = ref();
+onMounted(async () => {
+  pokemon.value = await store.loadPokemonDataByName(
+    route.params.pokemon.toString()
+  );
+  console.log('🚀 ~ onMounted ~ pokemon:', pokemon);
+});
+</script>
