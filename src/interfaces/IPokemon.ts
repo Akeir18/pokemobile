ab09e0487da7c75c9777c01b4189f994a2e37cda
Christian Jiménez Roche <christianjimenezroche@gmail.com>
2024-04-04 13:47:09 +0200

    Adding information for the pokemon page

--- src/interfaces/IPokemon.ts
+++ src/interfaces/IPokemon.ts
@@ -4,10 +4,22 @@ export interface IPokemon {
   names: IPokemonNames;
   types: Type[];
   sprites: Sprites;
-  height: number;
-  weight: number;
+  heightCm: number;
+  heightIn: number;
+  weightKg: number;
+  weightLbs: number;
   baseHappiness: number;
+  baseHappinessPercent: number;
+  experienceGained: number;
+  defaultPokemon: boolean;
   catchRate: number;
+  catchRatePercent: number;
+  genderRateMale: number;
+  genderRateFemale: number;
+  visualDifferences: boolean;
+  formChange: boolean;
+  hatchCycles: number;
+  isBaby: boolean;
 }
 
 export interface IPokemonNames {
